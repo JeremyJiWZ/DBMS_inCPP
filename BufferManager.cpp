aef1b51bdb34a3795a79801001c1b15ac06ea39e
JeremyJiWZ <mr.ji@icloud.com>
2015-10-29 00:48:23 +0800

    new buffer

--- BufferManager.cpp
+++ BufferManager.cpp
@@ -23,7 +23,7 @@ void BufferManager::addTime(string DB_Name) //遍历文件块，将其itime++
     }
 }
 
-blockInfo *BufferManager::findBlock(string DB_Name)
+blockInfo* BufferManager::findBlock(string DB_Name)//找到一个空块并将他返回;空块来自：垃圾链表、内存申请、替换块
 {
     blockInfo *block;
     block=BlockHandle;
@@ -54,23 +54,68 @@ blockInfo *BufferManager::findBlock(string DB_Name)
     }
     return block;
 }
-void replce(fileInfo *m_fileInfo,blockInfo *m_blockInfo)
+void BufferManager::replce(fileInfo *m_fileInfo,blockInfo *m_blockInfo)//找到文件最后一块，将该块链接到块尾
+{
+    m_blockInfo->file=m_fileInfo;
+    blockInfo *b=m_fileInfo->firstBlock;
+    if (b==NULL)    //该文件无块
+        m_fileInfo->firstBlock=m_blockInfo;
+    else
+    {
+        while (b->next!=NULL) {//找到最后一块
+            b=b->next;
+        }
+        b->next=m_blockInfo;
+    }
+}
+fileInfo* BufferManager::get_file_info(string DB_Name,string File_Name,int m_fileType)//得到文件名
+{
+    fileInfo* file=FileHandle;
+    while (file!=NULL) { //遍历文件链表，找到文件名与之相同的文件
+        if(file->fileName==File_Name)
+        {
+            return file;
+        }
+        file=file->Next;
+    }
+    //文件链表没有该文件，则重新构建
+    if (fileCount<5) { //直接从内存中申请,初始化，并作为fileHandle的头，返回file
+        file=new fileInfo;
+        file->busy=0;
+        file->FileBlockNum=0;
+        file->fileName=File_Name;
+        file->firstBlock=NULL;
+        file->Next=FileHandle;
+        FileHandle=file;
+        return file;
+    }
+    else//文件已满，选择一个文件来替换
+    {
+        file=FileHandle;
+        while (file->busy) { //找到一个文件的块未被使用的
+            file=file->Next;
+        }
+        closeFile(DB_Name, file, m_fileType);//关闭该文件，并把其中的块放进垃圾链表
+    }
+    //重新申请一个文件头，初始化并设为fileHandle的头
+    file=new fileInfo;
+    file->busy=0;
+    file->FileBlockNum=0;
+    file->fileName=File_Name;
+    file->firstBlock=NULL;
+    file->Next=FileHandle;
+    FileHandle=file;
+    return file;
+}
+void BufferManager::closeFile(string DB_Name, fileInfo *file, int type)
 {
     
 }
-fileInfo *get_file_info(string DB_Name,string File_Name,int m_fileType)
+void BufferManager::writeBlock(string DB_Name,blockInfo* block)
 {
     
 }
-void closeFile(string DB_Name,string File_Name,int m_fileType)
-{
-    
-}
-void writeBlock(string DB_Name,blockInfo* block)
-{
-    
-}
-blockInfo *readBlock(FILE *fp, int blockNum)
+blockInfo* BufferManager::readBlock(FILE *fp, int blockNum)
 {
     
 }
